24cfbc7858d74d63d717bd5deb6a2ec42ddc09b5
Marvin Cano <canomarvin01@gmail.com>
2026-01-31 01:47:36 +0100

    refactor: rebase DSL base language on CUDA instead of Metal. CUDA has better support in toolchain and VSCode.

---
+++ common.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "dsl.h"
+
+#ifndef UTILS_COMMON
+#define UTILS_COMMON
+
+// Maths
+#include "maths/transform.h"
+#include "maths/trigonometry.h"
+#include "maths/easing.h"
+
+// Image manipulation
+#include "image/coords.h"
+#include "image/2d.h"
+
+#endif // UTILS_COMMON
--- dsl.h
+++ dsl.h
@@ -1,6 +1,6 @@
 #pragma once
-// Select backend: define SUPPORT_CUDA for CUDA, otherwise Metal.
-#if defined(SUPPORT_CUDA)
+
+#if defined(__CUDACC__)
     #include "dsl_cuda.h"
 #else
     #include "dsl_metal.h"
